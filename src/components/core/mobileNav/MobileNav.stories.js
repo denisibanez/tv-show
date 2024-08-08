11517ca98320a16a95117985e48b04d4d1ce1bcf
denis.ibanez@outlook.com <denis.ibanez@outlook.com>
2024-08-09 00:44:46 +0100

    feature: storybook start configuration

---
+++ src/components/core/mobileNav/MobileNav.stories.js
@@ -0,0 +1,38 @@
+
+import { MobileNav } from './index';
+/* import storybook-vue3-router */
+import { vueRouter } from 'storybook-vue3-router'
+
+// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
+export default {
+  title: 'Components/Core/MobileNav',
+  component: MobileNav,
+  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+  argTypes: {
+   
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { MobileNav },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    return { args };
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`
+  template: '<MobileNav v-bind="args" @click.capture="onClick" />',
+});
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/vue/writing-stories/args
+Primary.args = {
+
+};
+
+/* adding storybook-vue3-router decorator */
+Primary.decorators = [
+  /* this is the basic setup with no params passed to the decorator */
+  vueRouter()
+]
---
+++ src/components/slider/SliderComponent.stories.js
@@ -0,0 +1,38 @@
+
+import { SliderComponent } from './index';
+/* import storybook-vue3-router */
+import { vueRouter } from 'storybook-vue3-router'
+
+// More on default export: https://storybook.js.org/docs/vue/writing-stories/introduction#default-export
+export default {
+  title: 'Components/Core/SliderComponent',
+  component: SliderComponent,
+  // More on argTypes: https://storybook.js.org/docs/vue/api/argtypes
+  argTypes: {
+   
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/vue/writing-stories/introduction#using-args
+const Template = (args) => ({
+  // Components used in your story `template` are defined in the `components` object
+  components: { SliderComponent },
+  // The story's `args` need to be mapped into the template through the `setup()` method
+  setup() {
+    return { args };
+  },
+  // And then the `args` are bound to your component with `v-bind="args"`
+  template: '<SliderComponent v-bind="args" @click.capture="onClick" />',
+});
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/vue/writing-stories/args
+Primary.args = {
+
+};
+
+/* adding storybook-vue3-router decorator */
+Primary.decorators = [
+  /* this is the basic setup with no params passed to the decorator */
+  vueRouter()
+]
