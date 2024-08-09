03519be0152761b45ecdc7f9a55898cbb9dcd6b8
denis.ibanez@outlook.com <denis.ibanez@outlook.com>
2024-08-09 16:43:23 +0100

    fix: mobile menu indez

--- src/components/core/mobileNav/MobileNav.vue
+++ src/components/core/mobileNav/MobileNav.vue
@@ -10,9 +10,10 @@
         ]"
         @click="handleClick(item)"
       >
-        <span v-if="item.notifications" class="badge"
-          ><span> {{ item.notifications }}</span></span
-        >
+        <span
+          v-if="item.notifications"
+          class="badge"
+        ><span> {{ item.notifications }}</span></span>
         <div class="icon">
           <span class="material-symbols-outlined buttonIcon__icon">
             {{ item.icon }}
@@ -88,7 +89,7 @@ const handleClick = (item: MenuNavInterface) => {
     position: fixed;
     bottom: 0;
     width: 100%;
-    z-index: 1;
+    z-index: 99;
 
     ul {
       li {
